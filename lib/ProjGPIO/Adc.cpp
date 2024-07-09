f8fe4dbc672b247d98d2608f67263992ff1eb3d0
hypercube <hypercube>
2024-07-09 16:49:01 +0200

    adc feudum

--- lib/ProjGPIO/Adc.cpp
+++ lib/ProjGPIO/Adc.cpp
@@ -2,26 +2,21 @@
 
 Adc::Adc(const gpio_num_t pin){
     _pin = pin;
-    _efuse = esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_DEFAULT_VREF)==ESP_OK;
-    if(_efuse){
-        esp_adc_cal_characterize(_pin<11?ADC_UNIT_1:ADC_UNIT_2, ADC_ATTEN_DB_12, ADC_WIDTH_BIT_12, 1100, &_efuse_chars);
-    }
-    if(_pin<11){
-        adc1_config_width(ADC_WIDTH_BIT_12);
-        adc1_config_channel_atten((adc1_channel_t)(_pin-1),ADC_ATTEN_DB_12);
-    }
-    else if(_pin<21){
-        adc2_config_channel_atten((adc2_channel_t)(_pin-1),ADC_ATTEN_DB_12);
-    } 
+
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT_1,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
+    adc_oneshot_chan_cfg_t config;
+    config.bitwidth = ADC_BITWIDTH_DEFAULT;
+    config.atten = ADC_ATTEN_DB_12;
+
+    adc_oneshot_new_unit(&init_config1, &_adc_handle);
+
+    adc_oneshot_config_channel(_adc_handle, (adc_channel_t)(_pin-1), &config);
 }
 int Adc::get(){
-    if(_efuse)
-        return esp_adc_cal_raw_to_voltage(adc1_get_raw((adc1_channel_t)(_pin-1)), &_efuse_chars);
-    else if(_pin<11)
-        return adc1_get_raw((adc1_channel_t)(_pin-1))/4095*2500;
-    else if(_pin<21){
-        int raw_out;
-        adc2_get_raw((adc2_channel_t)(_pin-1),ADC_WIDTH_BIT_12, &raw_out);
-        return raw_out;
-    }
+    int adc_raw;
+    adc_oneshot_read(_adc_handle, (adc_channel_t)(_pin-1), &adc_raw);
+    return adc_raw;
 }
--- lib/ProjGPIO/Adc.h
+++ lib/ProjGPIO/Adc.h
@@ -1,14 +1,15 @@
 #pragma once
 
 #include "driver/adc.h"
+#include "esp_adc/adc_oneshot.h"
 #include "esp_adc_cal.h"
 
 class Adc{
     public:
         gpio_num_t _pin;
-        bool _efuse;
-        esp_adc_cal_characteristics_t _efuse_chars;
-        Adc(const gpio_num_t pin);
+        adc_oneshot_unit_handle_t _adc_handle;
 
+        Adc(const gpio_num_t pin);
+        //returns 0 to 4095
         int get();
 };
